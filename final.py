05a717a588b5b4f7bdc9c2978aeecf8e66ea0829
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-04-05 11:59:57 -0400

    Added graphical distribution of MBTI personality types

--- final.py
+++ final.py
@@ -1,6 +1,17 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("final.csv")
 
+# Print dataset
 print(df.head)
+
+# Print distribution of MBTI personality types
+cnt_types = df['type'].value_counts()
+plt.figure(figsize=(12,4))
+sns.barplot(cnt_types.index, cnt_types.values, alpha=0.8)
+plt.ylabel('Number of Occurrences', fontsize=12)
+plt.xlabel('MBTI Personality Types', fontsize=12)
+plt.show()
