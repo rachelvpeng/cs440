4556f7bbde7dfd1242b7fe7c31e8b5194e4d22bf
Rachel Peng <rachelvpeng@gmail.com>
2020-04-11 00:59:53 -0700

    swiggity swaggity

--- final.py
+++ final.py
@@ -8,6 +8,9 @@ df = pd.read_csv("final.csv")
 # Print dataset
 print(df.head)
 
+# Check for null values
+print(df.isnull().sum())
+
 # Print distribution of MBTI personality types
 cnt_types = df['type'].value_counts()
 plt.figure(figsize=(12,4))
@@ -21,5 +24,46 @@ df['is_E'] = df['type'].apply(lambda x: 1 if x[0] == 'E' else 0)
 df['is_S'] = df['type'].apply(lambda x: 1 if x[1] == 'S' else 0)
 df['is_T'] = df['type'].apply(lambda x: 1 if x[2] == 'T' else 0)
 df['is_J'] = df['type'].apply(lambda x: 1 if x[3] == 'J' else 0)
-df.columns = ['type','is_E','is_S','is_T','is_J']
-df.head()
+# df.columns = ['type','is_E','is_S','is_T','is_J']
+print(df.head)
+
+# Visualize I, E classifiers
+labels = 'I', 'E'
+sizes = [8675 - np.count_nonzero(df['is_E']), np.count_nonzero(df['is_E'])]
+colors = ['gold', 'lightcoral']
+explode = (0.1, 0)  # explode 1st slice
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
+
+# Visualize N, S classifiers
+labels = 'N', 'S'
+sizes = [8675 - np.count_nonzero(df['is_S']), np.count_nonzero(df['is_S'])]
+colors = ['gold', 'lightcoral']
+explode = (0.1, 0)  # explode 1st slice
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
+
+# Visualize F, T classifiers
+labels = 'F', 'T'
+sizes = [8675 - np.count_nonzero(df['is_T']), np.count_nonzero(df['is_T'])]
+colors = ['gold', 'lightcoral']
+explode = (0.1, 0)  # explode 1st slice
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
+
+# Visualize P, J classifiers
+labels = 'P', 'J'
+sizes = [8675 - np.count_nonzero(df['is_J']), np.count_nonzero(df['is_J'])]
+colors = ['gold', 'lightcoral']
+explode = (0.1, 0)  # explode 1st slice
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
+
+# Visualize correlation
+sns.heatmap(df[['is_E', 'is_S', 'is_T', 'is_J']].corr(), annot = True, cmap = 'RdPu')
+plt.title('Correlation of Variables')
+plt.show()
